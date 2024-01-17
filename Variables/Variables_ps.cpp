905df0456fb4a6c310d1b2f432787beab96fbcba
ahmedtamee <123337830+tameesoul@users.noreply.github.com>
2024-01-18 01:07:57 +0200

    ez ps hw

---
+++ Variables/Variables_ps.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std; 
+
+
+
+int main()
+{
+    /*
+      p1# 
+    float n1,n2;
+    cin>>n1>>n2; 
+    cout<< n1 + n2<<" "<<n1 * n2 << " " << n1 / n2 <<" "<<n1 - n2; 
+    */
+   /*
+   p2#
+   string name1 , name2;
+   cin>>name1>>name2;
+   int id1 ,id2;
+   cin>>id1>>id2;
+   float grade1 , grade2; 
+   cin>>grade1 >>grade2;
+   cout<<"name of 1 student is :" <<name1<<"\n"<<"his id is :"<<id1<<"\n"<<"his grade is : "<<grade1<<"\n"; 
+   cout<<"name of 2 student is :" <<name2<<"\n"<<"his id is :"<<id2<<"\n"<<"his grade is : "<<grade2<<"\n";
+   cout<<"the average of this grades is "<<(grade1+grade2)/2<<"\n";  
+*/
+
+/*
+p3#
+int n1,n2,n3,n4,n5,n6,n7,n8;
+cout<<"even numbers only\n";
+    cin>>n2>>n4>>n6>>n8;
+    cout<<"odd numbers only\n";
+    cin>>n1>>n3>>n5>>n7;
+    cout<<n2+n4+n6+n8<<" "<<n1+n3+n5+n7;
+    */
+}
