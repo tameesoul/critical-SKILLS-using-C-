873c1d97d10d529ea4abe7d92633ede6d1181d3f
ahmedtamee <123337830+tameesoul@users.noreply.github.com>
2024-01-22 00:53:45 +0200

    hw#3

--- Operators/hm.cpp
+++ Operators/hm.cpp
@@ -20,13 +20,37 @@ int main()
 
 
     
-	int a = 1, b = 1, c;
+	// int a = 1, b = 1, c;
 
-	cout<<(c = a+b, a = b, b = c, /// 2 , a = 1 , b= 2
-		   c = a+b, a = b, b = c, /// 3 , a=2 , b=3
-		   c = a+b, a = b, b = c, /// 5 , a = 3 , b = 5
-		   c = a+b, a = b, b = c) 
-           <<endl;/// 8 , a = 5 , b =  8  
-	return 0;
+	// cout<<(c = a+b, a = b, b = c, /// 2 , a = 1 , b= 2
+	// 	   c = a+b, a = b, b = c, /// 3 , a=2 , b=3
+	// 	   c = a+b, a = b, b = c, /// 5 , a = 3 , b = 5
+	// 	   c = a+b, a = b, b = c) 
+    //        <<endl;/// 8 , a = 5 , b =  8  
+	// return 0;
+
+
+ 
+    int a = 210; /// 210 
+	a /= 2; // 210/2 
+	cout<<a<<"\n"; // 105
+
+	cout<<(a /= 3)<<"\n"; //35 
+	cout<<(a /= 5)<<"\n"; //7
+	cout<<(a /= 7)<<"\n"; //1 
+
+	cout<<(2+3) * (5-(-3)) / 5 / 8 <<"\n"; ///  -1  
+
+	a = 10;  /// 10 
+	cout<<a++ + 10<<"\n"; /// 11+10 = 21 
+	cout<<++a + 10<<"\n"; /// 11 + 10 = 21 
+	cout<<a-- + 10<<"\n"; // 11 +10 = 21 
+	cout<<--a + 10<<"\n"; // 10 +10 = 20 
+
+	int b = 20;   // 20 
+	cout<<a++ + ++b<<"\n"; /// 10 + 21 = 31 	// Don't code this way
+	cout<<a<<"\n"; // 11 
+	++a+=10; /// 12+10 22  	// Don't code this way
+	cout<<a<<"\n"; // 22 
 
 }
