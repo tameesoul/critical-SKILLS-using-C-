54a0d0162068c7a4b48c9e1f68a5e87730c809a4
ahmedtamee <123337830+tameesoul@users.noreply.github.com>
2024-01-26 15:16:20 +0200

    logical op hw

---
+++ Operators/hm2.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+// int a = 10, b = 20, c = 30, d = 40; // 10 , 20 , 30 , 40 ,
+
+// 	cout << (a + b == c) << "\n"; /// 30 == 30 ? 1 
+// 	cout << (a + b + c >= 2 * d) << "\n"; /// 60 >= 80 ? 0
+
+// 	cout << (a > 5 || d < 30) << "\n"; // 1 
+// 	cout << (a > 5 && d < 30) << "\n"; // 1
+// 	cout << (a <= b && b <= c) << "\n"; /// 1 
+
+// 	cout << (a > 5 && d < 30 || c - b == 10) << "\n"; /// 1 
+// 	cout << (a <= b && b <= c && c <= d) << "\n"; ///
+
+// 	cout << (a > 5 && d < 30 || c > d || d % 2 == 0) << "\n";
+// 	cout << (a > 5 && d < 30 || c > d && d % 2 == 0) << "\n";
+
+// 	cout << ( a == 10 || b != 20  && c != 30 || d != 40) << "\n";
+// 	cout << ((a == 10 || b != 20) && c != 30 || d != 40) << "\n";
+
+
+    // 	int nb, ng, nt;
+	// cin >> nb >> ng >> nt;
+
+	// // nb greater than 25
+	// cout << (nb > 25) << "\n";
+
+	// // ng less than or equal to 30
+	// cout << (ng <= 30) << "\n";
+
+	// // nb > 20 and nt > 2 or ng > 30 and nt > 4
+	// cout << (nb > 20 && nt > 2 || ng > 30 && nt > 4) << "\n";
+
+	// // Either nb < 60 or ng < 70
+	// cout << (nb < 60 || ng < 70) << "\n";
+
+	// // Neither nb >= 60 nor ng >= 70
+	// cout << (  !(nb >= 60) && !(ng >= 70) ) << "\n";
+
+	// // nb is 10 more students than ng
+	// cout << (nb == ng + 10) << "\n";
+
+	// // Difference between nb and ng is more than 10 or nt > 5
+	// cout << (nb - ng > 10 || nt > 5) << "\n";
+
+	// // Either nb is 10 more students than ng or ng is 15 more students than nb
+	// cout << (nb == ng + 10 || ng == nb + 15) << "\n";
+
+	// return 0;
+
+
+    
+}
