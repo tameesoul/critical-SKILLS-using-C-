952ab3f7c65751c10b5cf437927bf52feb8bf317
ahmedtamee <123337830+tameesoul@users.noreply.github.com>
2024-01-26 15:43:17 +0200

    ps2

--- Operators/hm3.cpp
+++ Operators/hm3.cpp
@@ -4,9 +4,27 @@ using namespace std;
 int main()
 {
 
-    int n1,n2,n3,n4,n5;
-    cin>>n1>>n2>>n3>>n4>>n5;
-    cout<<(n1+n2+n3+n4+n5)/5<<"\n";
-    cout<<float(n1+n2+n3)/(n4+n5)<<"\n";
-    cout<<(float(n1+n2+n3)/3)/float(((n4+n5))/2);
+    // int n1,n2,n3,n4,n5;
+    // cin>>n1>>n2>>n3>>n4>>n5;
+    // cout<<(n1+n2+n3+n4+n5)/5<<"\n";
+    // cout<<float(n1+n2+n3)/(n4+n5)<<"\n";
+    // cout<<(float(n1+n2+n3)/3)/float(((n4+n5))/2);
+
+
+/// p2
+
+double n1,n2;
+cin>>n1>>n2;
+
+double result = n1/n2;
+
+cout<<result - int(result);
+
+
+
+
+
+    // float n1,n2;
+    // cin>>n1>>n2;
+    // cout<<n1/n2<<"\n";
 }
