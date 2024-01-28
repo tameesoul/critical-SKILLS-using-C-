d100809cccef2fa95b9093b31789a4d347dbbdff
ahmedtamee <123337830+tameesoul@users.noreply.github.com>
2024-01-28 20:33:51 +0200

    ps#2

--- Selection/hm2.cpp
+++ Selection/hm2.cpp
@@ -3,14 +3,26 @@ using namespace std;
 
 int main(void)
 {
-    int n1 ,n2;
-    cin>>n1>>n2;
-    if(n1 %2 != 0 && n2 %2 != 0)
-    cout<<n1 * n2;
-    else if(n1 %2 ==0 && n2 %2 ==0)
-    cout<<n1/n2;
-    else if (n1 %2 !=0 && n2%2==0)
-    cout<<n1+n2;
+    // /// p1
+    // int n1 ,n2;
+    // cin>>n1>>n2;
+    // if(n1 %2 != 0 && n2 %2 != 0)
+    // cout<<n1 * n2;
+    // else if(n1 %2 ==0 && n2 %2 ==0)
+    // cout<<n1/n2;
+    // else if (n1 %2 !=0 && n2%2==0)
+    // cout<<n1+n2;
+    // else 
+    // cout<< n1 - n2;
+    //p2
+    int n1 , n2 , n3; //123
+    cin>>n1>>n2>>n3; // 123
+    if(n1> n2 && n1 > n3 && n2 > n3  ) 
+    cout << n3 << " " << n2 << " " << n1 << "\n";  
+    else if (n2>n1 && n2 >n3 && n1>n3)
+    {
+        cout<< n3 <<" " << n1 << " " << n2 <<"\n";
+    }
     else 
-    cout<< n1 - n2;
+    cout<< n1 << " " << n2 << " " << n3 <<"\n";
 }
