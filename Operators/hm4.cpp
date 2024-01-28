b4623945366e8406df8e7166d31090d88bfb1d26
ahmedtamee <123337830+tameesoul@users.noreply.github.com>
2024-01-28 03:21:52 +0200

    ps#3

--- Operators/hm4.cpp
+++ Operators/hm4.cpp
@@ -3,14 +3,17 @@ using namespace std;
 
 int main()
 {
-
+    ///ps2;
+    // int num; 
+    // cin>>num;
+    // int last1 = num %10;
+    // num /=10;
+    // int last2 = num %10;
+    // num/=10;
+    // int last3 = num%10;
+    // num/=10;
+    // cout<<last1+last2 +last3 <<" "<<num;
     int num; 
     cin>>num;
-    int last1 = num %10;
-    num /=10;
-    int last2 = num %10;
-    num/=10;
-    int last3 = num%10;
-    num/=10;
-    cout<<last1+last2 +last3 <<" "<<num;
+    cout<<(num/1000)%10;
 }
