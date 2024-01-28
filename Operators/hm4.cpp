0e5692d5b815d8981bef131fdffc44d727d52e04
ahmedtamee <123337830+tameesoul@users.noreply.github.com>
2024-01-28 03:12:22 +0200

    ps#2

--- Operators/hm4.cpp
+++ Operators/hm4.cpp
@@ -3,8 +3,14 @@ using namespace std;
 
 int main()
 {
+
     int num; 
     cin>>num;
-    bool is_even = (num % 2 == 0); 
-    cout<<is_even<<"\n;
+    int last1 = num %10;
+    num /=10;
+    int last2 = num %10;
+    num/=10;
+    int last3 = num%10;
+    num/=10;
+    cout<<last1+last2 +last3 <<" "<<num;
 }
