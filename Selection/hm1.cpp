fb7f5db2ab5caa5dcf2779dffaab6f26876317e2
ahmedtamee <123337830+tameesoul@users.noreply.github.com>
2024-01-28 20:10:25 +0200

    prac#3

--- Selection/hm1.cpp
+++ Selection/hm1.cpp
@@ -23,12 +23,25 @@ int main(void)
 	// 	cout<<"operation is not found";
 	// }
 
-	int n1,n2,n3; // 10 , 20 , 30 
-	cin>>n1>>n2>>n3; 
-	int ans = n1; //// 10 
-	if(ans > n2) /// 10 > 20 ?
-		ans = n2; //10
-	if(ans > n3) // 10 > 30 ? 
-	ans = n3; //10
-	cout<<ans; //10
+	//p2
+	// int n1,n2,n3; // 10 , 20 , 30 
+	// cin>>n1>>n2>>n3; 
+	// int ans = n1; //// 10 
+	// if(ans > n2) /// 10 > 20 ?
+	// 	ans = n2; //10
+	// if(ans > n3) // 10 > 30 ? 
+	// ans = n3; //10
+	// cout<<ans; //10
+
+	int n;
+	cin >>n;
+	if(n%2==0)
+	cout<<n%10<<"\n";
+	else if(n%2!=0 && n<1000)
+	cout<<n%100<<"\n";
+	else if(n%2!=0 && 1000>n<1000000)	
+	cout<<n%10000<<"\n";
+	else
+		cout<<-n<<"\n";
+	
 }
