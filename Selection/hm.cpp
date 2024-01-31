31647936fe00a48a3666c4d8626e3b68b300199a
ahmedtamee <123337830+tameesoul@users.noreply.github.com>
2024-01-31 16:32:35 +0200

    selection hard hw

--- Selection/hm.cpp
+++ Selection/hm.cpp
@@ -6,17 +6,35 @@ int main(void)
 {
 
     //ps1
-    int n1,n2,n3,n4,n5,n6,n7,n8,n9,n10; 
-    cin>>n1; int max = n1;
-    cin>>n2; if(n2>max) max = n2;
-    cin>>n3; if(n3>max) max = n3;
-    cin>>n4; if(n4>max) max = n4;
-    cin>>n5; if(n5>max) max = n5;
-    cin>>n6; if(n6>max) max = n6;
-    cin>>n7; if(n7>max) max = n7;
-    cin>>n8; if(n8>max) max = n8;
-    cin>>n9; if(n9>max) max = n9;
-    cin>>n10; if(n10>max) max = n10;
-    cout<<max;
+    // int num , result ;
+    // cin >> result; 
+    // cin>>num; if(num>result) result = num;
+    // cin>>num; if(num>result) result = num;
+    // cin>>num; if(num>result) result = num;
+    // cin>>num; if(num>result) result = num;
+    // cin>>num; if(num>result) result = num;
+    // cin>>num; if(num>result) result = num;
+    // cin>>num; if(num>result) result = num;
+    // cin>>num; if(num>result) result = num;
+    // cin>>num; if(num>result) result = num;
+    // cout<<result;
+
+    //ps2
+
+    int cnt , result , num;
+    cin>> cnt; 
+    cin>>result;
+    cnt-=1;
+    if(cnt>0) {cnt -=1; cin>>num; if(num>result)  result = num;} 
+    if(cnt>0) {cnt -=1; cin>>num; if(num>result)  result = num;} 
+    if(cnt>0) {cnt -=1; cin>>num; if(num>result)  result = num;} 
+    if(cnt>0) {cnt -=1; cin>>num; if(num>result)  result = num;} 
+    if(cnt>0) {cnt -=1; cin>>num; if(num>result)  result = num;} 
+    if(cnt>0) {cnt -=1; cin>>num; if(num>result)  result = num;} 
+    if(cnt>0) {cnt -=1; cin>>num; if(num>result)  result = num;} 
+    if(cnt>0) {cnt -=1; cin>>num; if(num>result)  result = num;} 
+    if(cnt>0) {cnt -=1; cin>>num; if(num>result)  result = num;} 
+    cout<<"\n";
+    cout<< result;
     return 0;
 }
---
+++ Selection/hm3.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(void)
+{
+    // ps1
+    // int num;
+    // cin>>num;
+    // int cnt=0;
+    // int s1 , e1,s2,e2,s3,e3;
+    // cin>>s1 >> e1; {if(num>s1 && num<e1) cnt+=1;}
+    // cin>>s2 >> e2; {if(num>s2 && num<e2) cnt+=1;}
+    // cin>>s3 >> e3; {if(num>s3 && num<e3) cnt+=1;}
+    // cout<<cnt;
+    // return 0 ;
+
+}
