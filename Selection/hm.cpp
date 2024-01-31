ec0744652e9913b7c52f692a7bbad4c1c278b9c6
ahmedtamee <123337830+tameesoul@users.noreply.github.com>
2024-01-31 13:08:37 +0200

    ps1

---
+++ Selection/hm.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+
+int main(void)
+{
+
+    //ps1
+    int n1,n2,n3,n4,n5,n6,n7,n8,n9,n10; 
+    cin>>n1; int max = n1;
+    cin>>n2; if(n2>max) max = n2;
+    cin>>n3; if(n3>max) max = n3;
+    cin>>n4; if(n4>max) max = n4;
+    cin>>n5; if(n5>max) max = n5;
+    cin>>n6; if(n6>max) max = n6;
+    cin>>n7; if(n7>max) max = n7;
+    cin>>n8; if(n8>max) max = n8;
+    cin>>n9; if(n9>max) max = n9;
+    cin>>n10; if(n10>max) max = n10;
+    cout<<max;
+    return 0;
+}
