3958b6375299564d58f4188725effedef47707b8
ahmedtamee <123337830+tameesoul@users.noreply.github.com>
2024-02-08 15:27:28 +0200

    dig numbers

--- loops/hm.cpp
+++ loops/hm.cpp
@@ -17,13 +17,28 @@ int main()
     // }
     // return 0;
 
-    int x , y ; //// 2 , 5 
-    cin>>x >> y;  /// 2, 5 
-    int res = 1;
-    while(y>0) /// 4  >0
+    // int x , y ; //// 2 , 5 
+    // cin>>x >> y;  /// 2, 5 
+    // int res = 1;
+    // while(y>0) /// 4  >0
+    // {
+    //     res *=x; /// x = x*x = 2*2 ;
+    //      y--; //// 4 
+    // }
+    // cout<<res;
+    int num;
+    cin>>num;
+    int dig = 0;
+    while (num>0)
     {
-        res *=x; /// x = x*x = 2*2 ;
-         y--; //// 4 
+        if(num <10)
+        {
+            cout<<1;
+            return 0;
+        }
+        dig +=2;
+        num /=10;
     }
-    cout<<res;
+    cout<<dig<<"\n";
+    
 }
