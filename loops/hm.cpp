d26bbe26276a17d2438f21f7b74c1000c332e67a
ahmedtamee <123337830+tameesoul@users.noreply.github.com>
2024-02-08 14:52:56 +0200

    power prac

--- loops/hm.cpp
+++ loops/hm.cpp
@@ -3,15 +3,27 @@ using namespace std;
 
 int main()
 {
-    int number; 
-    cin>>number;
-    while (number>0)
+
+    ////// p1 
+    // int number; 
+    // cin>>number;
+    // while (number>0)
+    // {
+    //     if(number % 3 == 0)
+    //     {
+    //         cout<<number<<"\n";
+    //     }
+    //     number --;
+    // }
+    // return 0;
+
+    int x , y ; //// 2 , 5 
+    cin>>x >> y;  /// 2, 5 
+    int res = 1;
+    while(y>0) /// 4  >0
     {
-        if(number % 3 == 0)
-        {
-            cout<<number<<"\n";
-        }
-        number --;
+        res *=x; /// x = x*x = 2*2 ;
+         y--; //// 4 
     }
-    return 0;
+    cout<<res;
 }
