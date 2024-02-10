77547ab82674f8f945323394f0197fbe152c0649
ahmedtamee <123337830+tameesoul@users.noreply.github.com>
2024-02-10 17:11:20 +0200

    p2

--- loops/hw3.cpp
+++ loops/hw3.cpp
@@ -4,14 +4,33 @@ using namespace std;
 int main()
 {
  
-    int n = 10; /// 10 
-    int a = 0 , b = 1; /// 0 , 1 
-    cout<<a << " "; /// 0 
-    for(int cnt = 2; cnt<=10;cnt++) /// 6 , 6<10
-    {
-        int c = a+b; /// 2 + 5 = 5
-        a = b; /// a = 5
-        b = c; /// b = 8 
-        cout<<c <<" "; /// 8  
-    } 
+     /// P1 
+    // int n = 10; /// 10 
+    // int a = 0 , b = 1; /// 0 , 1 
+    // cout<<a << " "; /// 0 
+    // for(int cnt = 2; cnt<=10;cnt++) /// 6 , 6<10
+    // {
+    //     int c = a+b; /// 2 + 5 = 5
+    //     a = b; /// a = 5
+    //     b = c; /// b = 8 
+    //     cout<<c <<" "; /// 8  
+    // }
+
+    /// P2
+//    int n, m , sum ;
+//     cin >> n >> m >> sum; 
+    
+//     int cnt = 0;
+
+//     for (int a = 1; a <= n; a++) {
+//         for (int b = 1; b <= m; b++) {
+//             if (a + b == sum) {
+//                 cnt++;
+//             }
+//         }
+//     }
+
+//     cout << cnt << "\n";
+
+    return 0;
 }
