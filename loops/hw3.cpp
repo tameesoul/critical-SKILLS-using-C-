e4227d5843c973010fff5c29305ebf73b067c1db
ahmedtamee <123337830+tameesoul@users.noreply.github.com>
2024-02-10 16:58:32 +0200

    fibonacci sequence

--- loops/hw3.cpp
+++ loops/hw3.cpp
@@ -1,16 +1,17 @@
 #include<iostream>
 using namespace std;
  
-int main() {
-	int t = 0;
-	for (int c = 0; c < 3; ++c) {
-		for (int var = 0; var < 4; ++var)
-			for (int h = 0; h < 2; ++h)
-				++t;
-			++t;
-		++t;
-	}
-	cout << t;
+int main()
+{
  
-	return 0;
+    int n = 10; /// 10 
+    int a = 0 , b = 1; /// 0 , 1 
+    cout<<a << " "; /// 0 
+    for(int cnt = 2; cnt<=10;cnt++) /// 6 , 6<10
+    {
+        int c = a+b; /// 2 + 5 = 5
+        a = b; /// a = 5
+        b = c; /// b = 8 
+        cout<<c <<" "; /// 8  
+    } 
 }
