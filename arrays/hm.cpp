8026278e9ec1f4f0890e615cbfcbdb0ffa5a6e67
ahmedtamee <123337830+tameesoul@users.noreply.github.com>
2024-02-14 11:55:03 +0200

    reversed array

--- arrays/hm.cpp
+++ arrays/hm.cpp
@@ -45,24 +45,44 @@ int main()
 
 
    //// same p3 but with the sum of the two values 
-    int n ,  arr[200] {};
-    cin>>n;
+    // int n ,  arr[200] {};
+    // cin>>n;
+    // for(int i = 0;i<n;i++)
+    // {
+    //     cin>>arr[i];
+    // }
+    // int max_1 =arr[0] , max_2 = arr[1];
+    // for(int i = 2;i<n;i++)
+    // {
+    //     if(arr[i]>max_1)
+    //     {
+    //         max_2 = max_1 ;
+    //         max_1 = arr[i];
+    //     }else if (arr[i]>max_2)
+    //     {
+    //         max_2 = arr[i];
+    //     }
+        
+    // }
+    // cout<<max_1 + max_2;
+
+    /// p4
+
+    int n ,arr[200] {};
+    cin>>n; /// 5
     for(int i = 0;i<n;i++)
     {
-        cin>>arr[i];
+        cin >> arr[i]; /// 10 ,20,30,40,50;
     }
-    int max_1 =arr[0] , max_2 = arr[1];
-    for(int i = 2;i<n;i++)
+    int temp; /// dec
+    for(int i = 0 ;i<n/2;++i) /// i =0;i < 5;
     {
-        if(arr[i]>max_1)
-        {
-            max_2 = max_1 ;
-            max_1 = arr[i];
-        }else if (arr[i]>max_2)
-        {
-            max_2 = arr[i];
-        }
-        
+        temp = arr[i]; // 
+        arr[i] = arr[n-i-1]; 
+        arr[n-i-1] = temp; 
+        cout<<arr[i] <<" ";
     }
-    cout<<max_1 + max_2;
+      for(int i = 0; i < n; ++i) {
+        cout << arr[i] << " ";
+    }
 }
