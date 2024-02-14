e6a2e29c2d8e1dd1653c2b71329cd2a1a113beeb
ahmedtamee <123337830+tameesoul@users.noreply.github.com>
2024-02-14 11:05:18 +0200

    ps3

--- arrays/hm.cpp
+++ arrays/hm.cpp
@@ -3,19 +3,43 @@ using namespace std;
 
 int main()
 {
-    const  int size = 5; //// 5
-    int arr[size];
+    //// p1
+    // const  int size = 5; //// 5
+    // int arr[size];
+    // for(int i = 0;i<size;i++)
+    // {
+    //     cin>>arr[i];
+    // }
+    // int min = arr[0];
+    // for(int i = 0;i<size;i++)
+    // {
+    //     if(arr[i]<min)
+    //     {
+    //         min = arr[i];
+    //     }
+    // }
+    // cout<<min;
+
+    //p2
+
+    int const size = 6;
+    int arr[size] {};
     for(int i = 0;i<size;i++)
     {
         cin>>arr[i];
     }
-    int min = arr[0];
-    for(int i = 0;i<size;i++)
+    int max_1 =arr[0] , max_2 = arr[1];
+    for(int i = 2;i<size;i++)
     {
-        if(arr[i]<min)
+        if(arr[i]>max_1)
         {
-            min = arr[i];
+            max_2 = max_1 ;
+            max_1 = arr[i];
+        }else if (arr[i]>max_2)
+        {
+            max_2 = arr[i];
         }
+        
     }
-    cout<<min;
+    cout<<max_1<<" " << max_2;
 }
