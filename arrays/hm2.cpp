e6269325a91904df8ea39ccd73075f17798afb54
ahmedtamee <123337830+tameesoul@users.noreply.github.com>
2024-02-17 22:50:00 +0200

    p2

--- arrays/hm2.cpp
+++ arrays/hm2.cpp
@@ -3,26 +3,57 @@ using namespace std;
 
 int main()
 {
-    int n , numbers[200]={};
-    cin>>n;
-    for(int i =0;i<n;i++)
-    {
-        cin>>numbers[i];
-    }
-    bool is_increase = true;
-    for(int i =1;i<n;i++)
-    {
-        if(numbers[i]<numbers[i-1])
-        {
-            is_increase = false;
-        }
-    }
-    if(is_increase)
-        cout<<"YES!"<<"\n";\
-        else
-        {
-            cout<<"NO"<<"\n";
-        }
-    
+    ///p1
+    // int n , numbers[200]={};
+    // cin>>n;
+    // for(int i =0;i<n;i++)
+    // {
+    //     cin>>numbers[i];
+    // }
+    // bool is_increase = true;
+    // for(int i =1;i<n;i++)
+    // {
+    //     if(numbers[i]<numbers[i-1])
+    //     {
+    //         is_increase = false;
+    //     }
+    // }
+    // if(is_increase)
+    //     cout<<"YES!"<<"\n";\
+    //     else
+    //     {
+    //         cout<<"NO!"<<"\n";
+    //     }
 
+    //p2
+    // int n , numbers[2000]={};
+    // cin>>n;
+    // for(int i = 0;i<n;i++)
+    // {
+    //     cin>>numbers[i];
+    // }
+    // int max = numbers[0];
+    // int min = numbers[0];
+    // int temp = 0;
+    // for(int i =0;i<n;i++)
+    // {
+    //     if(numbers[i]>max)
+    //     {
+    //         max = numbers[i];
+    //     }
+    //     if(numbers[i]<min)
+    //     {
+    //         min = numbers[i];
+    //     }
+    //     temp = min;
+    //     min = max;
+    //     max = temp;
+    //     cout<<numbers[i];
+    // }
+    // cout<<"\n" <<"\n";
+    // for(int i = 0;i<n;i++)
+    // {
+    //     cout<<numbers[i]<<"\n";
+    // }
+    // cout<<max<<" "<< min ;
 }
--- arrays/tempCodeRunnerFile.cpp
+++ arrays/tempCodeRunnerFile.cpp
@@ -1,20 +1,5 @@
- int const size = 6;
-    int arr[size] {};
-    for(int i = 0;i<size;i++)
-    {
-        cin>>arr[i];
-    }
-    int max_1 =arr[0] , max_2 = arr[1];
-    for(int i = 2;i<size;i++)
-    {
-        if(arr[i]>max_1)
-        {
-            max_2 = max_1 ;
-            max_1 = arr[i];
-        }else if (arr[i]>max_2)
-        {
-            max_2 = arr[i];
-        }
-        
-    }
-    cout<<max_1<<" " << max_2;
+ // cout<<"\n" <<"\n";
+    // for(int i = 0;i<n;i++)
+    // {
+    //     cout<<numbers[i]<<"\n";
+    // }
