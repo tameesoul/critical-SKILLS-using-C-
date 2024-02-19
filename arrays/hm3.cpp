936181f79f9f592608e1ae2e0afafc2061da8514
ahmedtamee <123337830+tameesoul@users.noreply.github.com>
2024-02-19 22:01:45 +0200

    keep in streal

--- arrays/hm3.cpp
+++ arrays/hm3.cpp
@@ -3,29 +3,5 @@ using namespace std;
 
 int main()
 {
-    int n , number[200];
-    cin>>n;
-    while(n>0)
-    {
-        int i;
-        cin>>number[i];
-        n--;
-    }
-    for(int i = 1;i<n;i++)
-    {
-        for(int j = 0; j<n-i-1;j++)
-        {
-            if(number[i]<number[i-1])
-            {
-               int temp = number[j];
-                number[j] = number[j + 1];
-                number[j + 1] = temp;
-            }
-        }
-    }
-
-    for(int i = 0;i<n;i++)
-    {
-        cout<<number[i]<<" "; 
-    }
+    cout<<"keep in streak";
 }
