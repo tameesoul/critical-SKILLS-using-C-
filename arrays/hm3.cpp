f049c7ed3300566b14fd6d794ae9407113d2df77
ahmedtamee <123337830+tameesoul@users.noreply.github.com>
2024-02-20 11:44:00 +0200

    some error in p2

--- arrays/hm3.cpp
+++ arrays/hm3.cpp
@@ -2,29 +2,58 @@
 using namespace std;
 
 int main() {
-    int n, numbers[200];
-    cin >> n;
+    // int n, numbers[200];
+    // cin >> n;
 
-    for (int i = 0; i < n; i++) {
-        cin >> numbers[i]; 
-    }
+    // for (int i = 0; i < n; i++) {
+    //     cin >> numbers[i]; 
+    // }
 
-    int min_1 = numbers[0], min_2 = numbers[1], min_3 = numbers[2];
+    // int min_1 = numbers[0], min_2 = numbers[1], min_3 = numbers[2];
 
-    for (int i = 1; i < n; i++) {
-        if (numbers[i] < min_1) {
-            min_3 = min_2;
-            min_2 = min_1;
-            min_1 = numbers[i];
-        } else if (numbers[i] < min_2) {
-            min_3 = min_2;
-            min_2 = numbers[i];
-        } else if (numbers[i] < min_3) {
-            min_3 = numbers[i];
-        }
-    }
+    // for (int i = 1; i < n; i++) {
+    //     if (numbers[i] < min_1) {
+    //         min_3 = min_2;
+    //         min_2 = min_1;
+    //         min_1 = numbers[i];
+    //     } else if (numbers[i] < min_2) {
+    //         min_3 = min_2;
+    //         min_2 = numbers[i];
+    //     } else if (numbers[i] < min_3) {
+    //         min_3 = numbers[i];
+    //     }
+    // }
 
-    cout << min_1 << " " << min_2 << " " << min_3;
+    // cout << min_1 << " " << min_2 << " " << min_3;
 
-    return 0;
+    // return 0;
+
+    // int n, numbers[500];
+    // cin>>n;
+    // for(int i = 0;i<n;i++)
+    // {
+    //     cin>>numbers[i];
+    // }
+    // cout<<"pls enter your quary for search \n";
+    // int Q[200];
+    // for(int i = 0;i<n;i++)
+    // {
+    //     cin>>Q[i];
+    //     int found_index = -1;
+    //     for(int j = 0; j<n;j++)
+    //     {
+    //         if(numbers[j]==Q[i])
+    //         {
+    //             found_index = j;
+    //             break;
+    //         }else 
+    //         {
+    //             cout<<"-1";
+    //         }
+    //     }
+
+    //     cout<<found_index;
+    // }
+
 }
+
