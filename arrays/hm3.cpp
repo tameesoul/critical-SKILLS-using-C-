b230ecdcd1102d637546dac8e8fa74efee09dbfc
ahmedtamee <123337830+tameesoul@users.noreply.github.com>
2024-02-20 11:25:35 +0200

    sorting array

--- arrays/hm3.cpp
+++ arrays/hm3.cpp
@@ -1,7 +1,30 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main()
-{
-    cout<<"keep in streak";
+int main() {
+    int n, numbers[200];
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> numbers[i]; 
+    }
+
+    int min_1 = numbers[0], min_2 = numbers[1], min_3 = numbers[2];
+
+    for (int i = 1; i < n; i++) {
+        if (numbers[i] < min_1) {
+            min_3 = min_2;
+            min_2 = min_1;
+            min_1 = numbers[i];
+        } else if (numbers[i] < min_2) {
+            min_3 = min_2;
+            min_2 = numbers[i];
+        } else if (numbers[i] < min_3) {
+            min_3 = numbers[i];
+        }
+    }
+
+    cout << min_1 << " " << min_2 << " " << min_3;
+
+    return 0;
 }
--- arrays/tempCodeRunnerFile.cpp
+++ arrays/tempCodeRunnerFile.cpp
@@ -1,5 +1,1 @@
- // cout<<"\n" <<"\n";
-    // for(int i = 0;i<n;i++)
-    // {
-    //     cout<<numbers[i]<<"\n";
-    // }
+max_1
