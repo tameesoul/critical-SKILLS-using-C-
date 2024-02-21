ff022e712029b3f2d968a7da7d8628146fc5ea03
ahmedtamee <123337830+tameesoul@users.noreply.github.com>
2024-02-21 23:32:00 +0200

    Concatenating_strings

--- char_array/hm.cpp
+++ char_array/hm.cpp
@@ -4,13 +4,25 @@ using namespace std;
 
 int main()
 {
-	string words[] {"Cpp", "is", "challenging!"};
- 
-	int len = 3;
-	string all;
-	for (int i = 0; i < 3; ++i)
-		all += words[i] + ",";
- 
-	cout<<all;
-	return 0;
+  string s1 , s2;
+  cin>>s1 >>s2;
+  int max_size = s1.size();
+  if(max_size<s2.size())
+  {
+    max_size = s2.size();
+  }
+
+  for(int i = 0;i<max_size;i++)
+  {
+    if(i<s1.size())
+    {
+        cout<<s1[i];
+    }
+    if(i<s2.size())
+    {
+        cout<<s2[i];
+    }
+
+    cout<<"\n";
+  }
 }
