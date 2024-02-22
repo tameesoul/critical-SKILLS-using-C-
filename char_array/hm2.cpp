cb383f274c961ce76f2a0892046113c3493cb703
ahmedtamee <123337830+tameesoul@users.noreply.github.com>
2024-02-22 13:36:25 +0200

    prefix_char

---
+++ char_array/hm2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    /// p1
+    // string line , str;
+    // cin >> line >> str;
+    // int size = str.size();
+    // bool is_prefix = true;
+    // for(int i = 0; i<size;i++)
+    // {
+    //     if(line[i]!=str[i])
+    //     {
+    //         is_prefix = false;
+    //         break;
+    //     }
+    // }
+    // if(is_prefix)
+    // {
+    //     cout<<"YES!";
+    // }else{
+    //     cout<<"NO!";
+    // }
+}
