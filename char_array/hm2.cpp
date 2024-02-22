cb01943ecfe7be048ffb0e1a0dd5152f582887de
ahmedtamee <123337830+tameesoul@users.noreply.github.com>
2024-02-22 13:55:01 +0200

    grouping

--- char_array/hm2.cpp
+++ char_array/hm2.cpp
@@ -25,22 +25,43 @@ int main()
 
 
     // p2
-    string line , str;
-    cin >> line >> str;
-    int size = str.size();
-    bool is_suffix = true;
-    for(int i = 0;i<size;i++)
+    // string line , str;
+    // cin >> line >> str;
+    // int size = str.size();
+    // bool is_suffix = true;
+    // for(int i = 0;i<size;i++)
+    // {
+    //     if(line[i-size-1] == str[i-size-1])
+    //     {
+    //         is_suffix = false;
+    //     }
+    // }
+    // if(is_suffix)
+    // {
+    //     cout << "YES!";
+    // }
+    // else{
+    //     cout<<"NO!";
+    // }
+
+    /// p5;
+    // string num;
+    // cin >> num;
+    // int conv = (int) num;
+    // int res = (int)num * 3;
+
+    // cout<<conv << " " << res <<"\n";
+
+    /// p6;
+    string line;
+    cin>>line;
+    int size = line.size();
+    for(int i = 0; i<size;i++)
     {
-        if(line[i-size-1] == str[i-size-1])
+        cout<<line[i];
+        if(line[i] !=line[i+1])
         {
-            is_suffix = false;
+            cout<<" ";
         }
-    }
-    if(is_suffix)
-    {
-        cout << "YES!";
-    }
-    else{
-        cout<<"NO!";
     }
 }
