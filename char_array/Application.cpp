58bb4731b11acf0d6e69e6fd27496429c02e1735
ahmedtamee <123337830+tameesoul@users.noreply.github.com>
2024-02-24 19:26:46 +0200

    using function to reduce duplication v1

--- char_array/Application.cpp
+++ char_array/Application.cpp
@@ -1,103 +1,77 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    cout<<"1-Add new Employee\n";
-    cout<<"2-print all Employee\n";
-    cout<<"2-Delete by age\n";
-    cout<<"3-edit salary by name\n";
-    int choice;
-    int age[100] = {0};
-    float salary[100]={0};
-    char Gender [100];
-    string name[100];
-    int cnt =  0;
-    cin >> choice; 
-    if(choice == 1)
-    {
-        for(int i = 0;i<1;i++)
-        {
-            cout<<"Enter Employee Name:"<< " ";cin>>name[0];cout<<"\n"; 
-            cout<<"Enter Employee salary:"<< " ";cin>>salary[0];cout<<"\n";
-            cout<<"Enter Employee gender:"<< " ";cin>>Gender[0];cout<<"\n";
-            cout<<"Enter Employee age:"<< " ";cin>>age[0];cout<<"\n";
-            cnt++;
+const int MAX_EMPLOYEES = 100;
+
+// Function to add a new employee
+void addEmployee(string name[], int age[], float salary[], char Gender[], int& cnt) {
+    cout << "Enter Employee Name: "; cin >> name[cnt]; cout << "\n";
+    cout << "Enter Employee salary: "; cin >> salary[cnt]; cout << "\n";
+    cout << "Enter Employee gender: "; cin >> Gender[cnt]; cout << "\n";
+    cout << "Enter Employee age: "; cin >> age[cnt]; cout << "\n";
+    cnt++;
+}
+
+// Function to print all employees
+void printEmployees(string name[], int age[], float salary[], char Gender[], int cnt) {
+    for (int i = 0; i < cnt; i++) {
+        cout << name[i] << "\n";
+        cout << age[i] << "\n";
+        cout << salary[i] << "\n";
+        cout << Gender[i] << "\n";
+    }
+}
+
+// Function to edit salary by name
+void editSalaryByName(string name[], float salary[], int cnt) {
+    string QName;
+    cin >> QName;
+    for (int i = 0; i < cnt; i++) {
+        if (QName == name[i]) {
+            int new_salary;
+            cin >> new_salary;
+            salary[i] = new_salary;
             break;
-        } 
-    }
-    int end_prog;
-    cout<<"ADDED SUCCESSFULLY, TO END THE PROGRAM PRESS 0 to continue press 1\n";
-    cin >> end_prog;
-    if(end_prog == 0)
-    {
-        return 0;
-    }else(end_prog ==1);
-    {
-    cout<<"1-Add new Employee\n";
-    cout<<"2-print all Employee\n";
-    cout<<"3-Delete by age\n";
-    cout<<"4-edit salary by name\n";
-    }
-    cin>>choice;
-    if(choice == 2)
-    {
-        for(int i =0;i<cnt;i++)
-        {
-            cout<<name[i]<<"\n";
-            cout<<age[i]<<"\n";
-            cout<<salary[i]<<"\n";
-            cout<<Gender[i]<<"\n";
         }
     }
-      cout<< "TO END THE PROGRAM PRESS 0 to continue press 1\n";
-    cin >>end_prog;
-    if(end_prog == 0)
-    {
-        return 0;
-    }else(end_prog ==1);
-    {
-    cout<<"1-Add new Employee\n";
-    cout<<"2-print all Employee\n";
-    cout<<"3-Delete by age\n";
-    cout<<"4-edit salary by name\n";
-    }
-    if(choice == 4)
-    {
-        string QName;
-        cin>>QName;
-        for(int i =0;i<cnt;i++)
-        {
-            if(QName == name[i])
-            {
-                int new_salary; 
-                cin>>new_salary;
-                salary[i] = new_salary;
+}
+
+int main() {
+    int choice;
+    int age[MAX_EMPLOYEES] = { 0 };
+    float salary[MAX_EMPLOYEES] = { 0 };
+    char Gender[MAX_EMPLOYEES];
+    string name[MAX_EMPLOYEES];
+    int cnt = 0;
+
+    do {
+        cout << "1-Add new Employee\n";
+        cout << "2-Print all Employee\n";
+        cout << "3-Delete by age\n";
+        cout << "4-Edit salary by name\n";
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                addEmployee(name, age, salary, Gender, cnt);
                 break;
-            }
+            case 2:
+                printEmployees(name, age, salary, Gender, cnt);
+                break;
+            case 3:
+                // Implement delete by age functionality
+                break;
+            case 4:
+                editSalaryByName(name, salary, cnt);
+                cout << "Updated SUCCESSFULLY\n";
+                break;
+            default:
+                cout << "Invalid choice\n";
         }
-    }
-    cout<<"updated SUCCESSFULLY, TO END THE PROGRAM PRESS 0 to continue press 1\n";
-    cin >> end_prog;
-    if(end_prog == 0)
-    {
-        return 0;
-    }else(end_prog ==1);
-    {
-    cout<<"1-Add new Employee\n";
-    cout<<"2-print all Employee\n";
-    cout<<"2-Delete by age\n";
-    cout<<"3-edit salary by name\n";
-    }
-    cin>>choice;
-    if(choice == 2)
-    {
-        for(int i =0;i<cnt;i++)
-        {
-            cout<<name[i]<<"\n";
-            cout<<age[i]<<"\n";
-            cout<<salary[i]<<"\n";
-            cout<<Gender[i]<<"\n";
-        }
-    }
+
+        cout << "TO END THE PROGRAM PRESS 0, to continue press 1\n";
+        cin >> choice;
+    } while (choice != 0);
+
+    return 0;
 }
