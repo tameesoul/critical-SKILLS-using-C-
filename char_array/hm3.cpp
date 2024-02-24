5f15b043420bd1bdb74433f5f446197ea61a9c79
ahmedtamee <123337830+tameesoul@users.noreply.github.com>
2024-02-24 18:02:08 +0200

    dub

---
+++ char_array/hm3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string str; /// aabbee
+    cin >> str;  
+    str += "$";
+    int group_Size = 1; /// 1 
+
+    for (int i = 1; i < (int)str.size(); i++) {
+        if (str[i - 1] != str[i]) { /// 1-1= 0 > a != 1 = 1 no
+            if (i > 1) {
+                cout << "_";
+            }
+            cout << str[i - 1] << group_Size;
+            group_Size = 0;
+        }
+        ++group_Size; /// 2 
+    }
+
+    return 0;
+}
