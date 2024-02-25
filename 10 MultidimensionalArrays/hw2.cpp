4bc909a44bfbd7500d750497f1d3116572b1e7c9
ahmedtamee <123337830+tameesoul@users.noreply.github.com>
2024-02-26 00:23:40 +0200

    MAX_POS_WITH_VALUE

--- 10 MultidimensionalArrays/hw2.cpp
+++ 10 MultidimensionalArrays/hw2.cpp
@@ -3,22 +3,51 @@ using namespace std;
 
 int main()
 {
-    int position[2][2] = {0};
-    int x , y;
-    cin >> x >> y;
-    for(int row = 0;row<2;++row)
+    // int position[2][2] = {0};
+    // int x , y;
+    // cin >> x >> y;
+    // for(int row = 0;row<2;++row)
+    // {
+    //     for(int col = 0;col <3;col++)
+    //     {
+    //         cin >> position[row][col];
+    //     }
+    // }
+    // for(int i = 0;i<x;i++)
+    // {
+    //     cout <<"("<<i<<","<<position[i]<<")"<<" "; 
+    //     for(int j = 0;j<y;j++)
+    //     {
+    //         cout <<"("<<j<<","<<position[j]<<")"<<" "; 
+    //     }
+    // }
+
+    int row , col;
+    cin >> row >> col;
+    int MAX_NUM [200][200] = {0};
+    for(int i = 0;i<row;i++)
     {
-        for(int col = 0;col <3;col++)
+        for(int j =0;j<col;j++)
         {
-            cin >> position[row][col];
+            cin >>MAX_NUM[i][j];
         }
     }
-    for(int i = 0;i<x;i++)
+
+    int MAX = MAX_NUM[0][0];
+
+   for(int i = 1;i<row;i++)
     {
-        cout <<"("<<i<<","<<position[i]<<")"<<" "; 
-        for(int j = 0;j<y;j++)
+        for(int j =0;j<col;j++)
         {
-            cout <<"("<<j<<","<<position[j]<<")"<<" "; 
+            if(MAX_NUM[i][j]>MAX)
+            {
+                MAX = MAX_NUM[i][j];
+             cout <<"******************************************\n";
+                cout<< i  << " "<< j <<"\n" ;
+            }
         }
+
     }
+    cout <<"******************************************\n";
+    cout<<MAX<<"\n";
 }
