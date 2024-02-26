5db9ecf69888a155c9927d9021c92566c20afa02
ahmedtamee <123337830+tameesoul@users.noreply.github.com>
2024-02-26 11:41:30 +0200

    special_print

--- 10 MultidimensionalArrays/hw2.cpp
+++ 10 MultidimensionalArrays/hw2.cpp
@@ -22,32 +22,78 @@ int main()
     //     }
     // }
 
-    int row , col;
-    cin >> row >> col;
-    int MAX_NUM [200][200] = {0};
-    for(int i = 0;i<row;i++)
+
+//// P1
+//     int row , col;
+//     cin >> row >> col;
+//     int MAX_NUM [200][200] = {0};
+//     for(int i = 0;i<row;i++)
+//     {
+//         for(int j =0;j<col;j++)
+//         {
+//             cin >>MAX_NUM[i][j];
+//         }
+//     }
+
+//     int MAX = MAX_NUM[0][0];
+
+//    for(int i = 1;i<row;i++)
+//     {
+//         for(int j =0;j<col;j++)
+//         {
+//             if(MAX_NUM[i][j]>MAX)
+//             {
+//                 MAX = MAX_NUM[i][j];
+//              cout <<"******************************************\n";
+//                 cout<< i  << " "<< j <<"\n" ;
+//             }
+//         }
+
+//     }
+//     cout <<"******************************************\n";
+//     cout<<MAX<<"\n";
+
+//// P2;
+
+int rows , cols ;
+cin >> rows >> cols;
+int MAT[100][100];
+for(int i = 0; i<rows;i++)
+{
+    for(int j = 0; j<cols;j++)
     {
-        for(int j =0;j<col;j++)
-        {
-            cin >>MAX_NUM[i][j];
-        }
+        cin >> MAT[i][j];
     }
+}
+int i = 0 , j = 0;
+int left_diaganol = 0;
 
-    int MAX = MAX_NUM[0][0];
+while (i<rows && j<cols)
+{
+    left_diaganol +=MAT[i++][j++];
+}
+int right_diagonal = 0;
+i = 0 , j = cols -1;
+while(i<rows && j<cols)
+{
+    right_diagonal += MAT[i++][j--];
+}
 
-   for(int i = 1;i<row;i++)
-    {
-        for(int j =0;j<col;j++)
-        {
-            if(MAX_NUM[i][j]>MAX)
-            {
-                MAX = MAX_NUM[i][j];
-             cout <<"******************************************\n";
-                cout<< i  << " "<< j <<"\n" ;
-            }
-        }
+int last_row = 0;
+j = 0;
+while (j<cols)
+{
+    last_row += MAT[rows-1][j++];
+}
+int last_col = 0;
+i = 0;
+while(i<rows)
+{
+    last_col +=MAT[i++][cols-1]; 
+}
 
-    }
-    cout <<"******************************************\n";
-    cout<<MAX<<"\n";
+cout<<"left_diaganol = " << left_diaganol <<" "
+<<"right_diaganol = " << right_diagonal << " "
+<<"last_col ="  << last_col <<" "
+<< "last_row = "<<last_row<<"\n";  
 }
