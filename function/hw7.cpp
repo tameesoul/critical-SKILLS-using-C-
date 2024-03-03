533b23dd52c90f234027af804701bc82746bdfe6
ahmedtamee <123337830+tameesoul@users.noreply.github.com>
2024-03-03 14:45:19 +0200

    is_prime

--- function/hw7.cpp
+++ function/hw7.cpp
@@ -5,16 +5,35 @@ using namespace std;
 
 bool is_prime(int n)
 {
-    bool is_prime = false;
-    if(n % 2 == 0)
+    if(n == 1)
+    return false;
+    for(int i = 0;i<n;i++)
     {
-        is_prime = true;
+        if(n % i == 0)
+        {
+            return false;
+        }
+        return true;
     }
-    return is_prime;
 }
 
 
+int nth_prime(int n)
+{
+    for(int i =2;;++i)
+    {
+        if(is_prime(i))
+        --n;
+        if(n==0)
+        return i;
+    }
+    return -1;
+}
+
 int main()
 {
-
+    for(int i = 1;i<20;i++)
+    {
+        cout<<nth_prime(i)<<" ";
+    }
 }
