be6731c47102e99448a1787aa43ba8a821ccf1f6
ahmedtamee <123337830+tameesoul@users.noreply.github.com>
2024-03-07 16:36:18 +0200

    keepinstreak

--- 12 Recursive Functions/3n_plus_1.cpp
+++ 12 Recursive Functions/3n_plus_1.cpp
@@ -1,18 +1,16 @@
 #include<iostream>
+#include <cmath>
 using namespace std;
-
-void print_3n_plus(int n)
-{
-    cout<<n<<" "; // 
-    if(n == 1) // 
-    {return ;} 
-    if(n %2 ==0) //
-    { print_3n_plus(n/2);} // 
-    else
-     {print_3n_plus(3*n+1);}
-}
-
-int main()
-{
-   print_3n_plus(10);
+int main() {
+    float A, B, C, X;
+    cout<<"A=?   ";
+    cin>> A;
+    cout<<"B=?   ";
+    cin>> B;
+    cout<<"C=?  ";
+    cin>> C;
+    X=5*pow(A,2)*B*pow(C,3)+8*A*pow(B,2)*pow(C,2)-4*pow(B,3)*C;
+    cout<<endl;
+    cout<<"X=  "<<X<<endl;
+return 0;
 }
--- 12 Recursive Functions/hw4.cpp
+++ 12 Recursive Functions/hw4.cpp
@@ -3,18 +3,7 @@ using namespace std;
 
 void npluso(int n)
 {
-    cout<<n<<" "; // 6 , 3 
-    if(n == 1) // 6 != 1  , 3 != 1
-    {
-        return;
-    }
-    if(n % 2 == 0 ) /// !=
-    {
-        npluso(n/2); /// 3 
-    }
-    else{
-       npluso(3*n+1);  /// 3 * 3
-    }
+  
 }
 int main()
 {
