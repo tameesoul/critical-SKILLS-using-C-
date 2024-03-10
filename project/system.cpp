4198ea0f3fa536b6d4a8b26e4311521a2b7b802d
ahmedtamee <123337830+tameesoul@users.noreply.github.com>
2024-03-10 23:28:24 +0200

    hospital_system#v1

--- project/system.cpp
+++ project/system.cpp
@@ -4,15 +4,35 @@ using namespace std;
 string name[100];
 int specialization[15];
 bool status[100];
-int cnt = 1;
+int cnt = 0;
+void next_patient()
+{
+    cin>>specialization[cnt];
+    for(int i = 0;i<cnt;i++)
+    {
+        if(specialization[i] == specialization[cnt])
+        cout<<name[i]<<"\n";
+        break;
+    }
+}
 void add_new()
 {
     cout<<"Enter the specialization for the patient"<<" "; cin>>specialization[cnt]; cout<<"\n";
     cout<<"Enter the name of the patient"<<" "; cin>>name[cnt];cout<<"\n";
     cout<<"Enter the patient status"<<" "; cin>>status[cnt];cout<<"\n";
     cnt ++;
-
     cout<<"added successfully\n";
+}
+void printing_patients()
+{
+    for(int i = 0;i<cnt;i++)
+    {
+        if(status[i]==1)
+        {
+        }
+        cout<<name[i]<<" " << specialization[i]<< " " <<status[i]<<" "<<"\n";
+
+    }
 }
 
 int menu()
@@ -42,7 +62,21 @@ void system()
          {
             add_new();
          }
+         else if (choice ==2)
+         {
+            printing_patients();
+         }
+         else if (choice ==3)
+         {
+            next_patient();
+         }
+         else if(choice ==4)
+         {
+            break;
+         }
     }
+
+
 }
 int main()
 {
