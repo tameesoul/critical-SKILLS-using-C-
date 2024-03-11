bd0e061b693d21ebf406b68712c7e2f173e5f30e
ahmedtamee <123337830+tameesoul@users.noreply.github.com>
2024-03-11 22:15:55 +0200

    using function within struct

--- structures/practise.cpp
+++ structures/practise.cpp
@@ -7,6 +7,16 @@ struct employee
 	int age;
 	double salary;
 	char gender;
+
+
+	void read_employee()
+	{
+		cin >> name >> age >> salary >> gender;
+	}
+	void print_employee()
+	{
+		cout<< name<<" "<<age<<" " <<salary<<" " << gender; 
+	}
 };
 
 const int MAX = 1000;
@@ -15,25 +25,17 @@ employee employee_arr[MAX];
 int added = 0;
 
 
-void add_employee()
-{
-	cout<<"enter info about employee\n"; 
-	cin>>employee_arr[added].name >> employee_arr[added].age >> employee_arr[added].salary
-	>> employee_arr[added].gender;
-	added++;
-}
 void print_emp()
 {
-	for(int i =0;i<added;i++)
+	for(int i = 0;i<added;i++)
 	{
-		employee e = employee_arr[i]; 
-		cout<<e.name<<" " <<e.age << e.salary <<" " <<e.gender;
-
+		employee_arr[i].print_employee();
 	}
 }
+
 int main()
 {
-	add_employee();
+	employee_arr[added++].read_employee();
 	print_emp();
 }
 
