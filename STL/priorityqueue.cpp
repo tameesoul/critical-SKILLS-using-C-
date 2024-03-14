cdfc2a7f14f3ee2c8b7a113eba040df1fbfb5f3e
ahmedtamee <123337830+tameesoul@users.noreply.github.com>
2024-03-14 15:51:46 +0200

    priority_queue_usingSTL

--- STL/priorityqueue.cpp
+++ STL/priorityqueue.cpp
@@ -2,24 +2,23 @@
 #include<queue>
 using namespace std;
 
-void print(priority_queue<int> &pq) {
-	cout << "Priority Queue elements: ";
-	while (!pq.empty()) {
-		cout << pq.top() << " ";
-		pq.pop();
-	}
-	cout << "\n";
+void print(priority_queue<int>&pq)
+{
+    while (!pq.empty())
+    {
+        cout<<pq.top()<<" ";
+        pq.pop();
+    }
 }
-int main() {
-	// order from big to small
-	priority_queue<int> pq;
-	pq.push(3);	// 3
-	pq.push(5);	// 5 3
-	pq.push(1);	// 5 3 1
-	pq.push(7); // 7 5 3 1
-	pq.push(0); // 7 5 3 1 0
 
-	print(pq);
-	// use -ve number to sort small to large
-	return 0;
+int main()
+{
+    priority_queue<int> pq;
+
+    pq.push(6);
+    pq.push(10);
+    pq.push(6);
+    pq.push(4);
+    pq.push(8);
+    print(pq);
 }
