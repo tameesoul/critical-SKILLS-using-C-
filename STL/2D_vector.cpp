c8a5b90a8f3f89fa446407953b655b21cafde504
ahmedtamee <123337830+tameesoul@users.noreply.github.com>
2024-03-26 03:16:05 +0200

    2d_vector

--- STL/2D_vector.cpp
+++ STL/2D_vector.cpp
@@ -5,25 +5,27 @@ using namespace std;
 
 void t1()
 {
-    vector<string> v  {"ahmed","tamer","me"};
-    // for(auto val:v)
-    // {
-    //     cout<<val<<" "
-    // }
-    // int sz = v.size();
-    // for(auto i = 0;i<sz;i++)
-    // {
-    //     for(auto j = 0;j<sz;j++)
-    //     cout<<v[i][j]<<" ";
-    // }
-    cout<<v[3][3];
+    vector<string> v = {"ahmed","tamee","me"};
+    for(auto val : v)
+    cout<<v[0][0]<<" "; //a a a;
+
 }
 
 void t2()
 {
-    vector<vector<int>> v {};
+    vector<int> col (5,1);  //////
+    vector<vector<int>>row(3,col);
+    row[1][0].push_back(5);
+    for(auto val:row)
+    {
+        for(auto val2: col)
+        cout<<val2<<" ";
+        cout<<"\n";
+    }
 }
 int main()
 {
-    t1();
+    //  t1();
+
+    t2();
 }
