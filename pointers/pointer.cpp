2a09fc068b46c98fe079075fa74d221a3327e15c
ahmedtamee <123337830+tameesoul@users.noreply.github.com>
2024-05-05 04:37:55 +0300

    pointers

--- pointers/pointer.cpp
+++ pointers/pointer.cpp
@@ -1,16 +1,40 @@
-#include<iostream>
-using namespace std;
+    #include<iostream>
+    using namespace std;
+     
+    int main() 
+    {
+        // int x = 6;
+        // cout<< x << "\n"; //6
+        // int *ptr = &x;  // 
+        // cout<<ptr<<"\n"; // address
+        // cout<<*ptr<<"\n"; /// 6
+        // ptr+=5;
+        // cout<<ptr<<"\n"; // address
+        // cout<<*ptr<<"\n"; /// new  address
 
-int main()
-{
-    int age {25};
+        // char msg[] = {'h','e','l','l','o'};
+        // for(auto x : msg)
+        // cout<<x <<""; //hello
+        // cout<<"\n";
+        // char *ptr = msg; //// not &msg because msg is already pointer assign to the first address in the array
+        // cout<<*ptr<<"\n"; //h
+        // *ptr = 'H';
+        // cout<<*ptr<<"\n"; // H
+        // *ptr++;
+        //  *ptr = 'E';
+        // cout<<*ptr<<"\n";
+        //  for(auto x : msg)
+        // cout<<x <<"";
 
-    //cout<<age << " " << &age;
-  //  cout<<"#######################";
-    int &ref =age;
-    cout<< ref<< " "<<&ref<<"\n";  
-    ///// 25 , 0x61ff08S
+        int arr[5]  = {1,4,5,6,7};
 
-    int &ref2 = ref;
-    cout<<ref2 <<" "<< &ref2<<"\n";
-}
+        for(int i=0;i<5;i++)
+        {
+            cout<<arr[i]<<" "; //1 4 5 6 7
+        }
+        cout<<"\n";
+        for(int i = 0;i<5;i++)
+        {
+            cout<<*(arr+i) <<" "; /// 1 4 5 6 7
+        }
+    }
