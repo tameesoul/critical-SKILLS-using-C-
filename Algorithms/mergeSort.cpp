386c01751d2812a6a241c2314b8f9e0625f40b30
ahmedtamee <123337830+tameesoul@users.noreply.github.com>
2024-05-20 18:02:44 +0300

    mergeSortUsingVector

--- Algorithms/mergeSort.cpp
+++ Algorithms/mergeSort.cpp
@@ -1,73 +1,81 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-
-void merge(int array [] , int start , int midpoint , int end)
+void merge(vector<int>&items, int start, int mid, int end)
 {
-    int i , j , k;
-
-    int left_length = midpoint -start+1;
-    int right_length = end - midpoint;
-
-    int left_array [left_length];
-    int right_array [right_length];
-
-   for(int i=0; i < left_length; i++){
-        left_array[i] = array[start + i];
-    }
-    for(int j=0; j < right_length; j++){
-        right_array[j] = array[midpoint + 1 + j];
-    }
-
-    i = 0 , j = 0;
-    k = start;
-    while(i<left_length && j <right_length)
+    int left_length = mid - start + 1;
+    int right_length = end - mid;
+    vector<int> left_array(left_length);
+    vector<int> right_array(right_length);
+    for (int i = 0; i < left_length; i++)
     {
-        if(left_array[i]<=right_array[j])
+        left_array[i] = items[start + i];
+    }
+    for (int j = 0; j < right_length; j++)
+    {
+        right_array[j] = items[mid + 1 + j];
+    }
+    int i = 0;
+    int j = 0;
+    int k = start;
+    while (i < left_length && j < right_length)
+    {
+        if (left_array[i] <= right_array[j])
         {
-            array[k] = left_array[i];
+            items[k] = left_array[i];
             i++;
-        }else
-        {
-         array[k] = right_array[j];
-         j++;
         }
-
-        
+        else
+        {
+            items[k] = right_array[j];
+            j++;
+        }
         k++;
     }
-    while (i<left_length)
+    while (i < left_length)
     {
-        array[k] = left_array[i];
+        items[k] = left_array[i];
         i++;
         k++;
     }
-    while (j<right_length)
+    while (j < right_length)
     {
-        array[k] = right_array[j];
+        items[k] = right_array[j];
         j++;
         k++;
     }
-
-    
 }
-void mergeSort(int array[] , int start, int end)
+
+void mergeSort(vector<int> &items, int start, int end)
 {
-    if(start >= end){return;}
-    
-    int midpoint = (end + start)/2;
-    mergeSort(array, start, midpoint);
-    mergeSort(array, midpoint+1, end);
-    merge(array, start, midpoint, end);
+    if (end <= start)
+    {
+        return;
+    }
+    int mid = (end + start) / 2;
+    mergeSort(items, start, mid);
+    mergeSort(items, mid + 1, end);
+    merge(items, start, mid, end);
 }
 
 int main()
 {
-    int array [] = {8,5,2,4};
-    mergeSort(array, 0, 3);
-    for(int i = 0 ; i<4 ; i++)
+    int n;
+    cin>>n;
+    vector<int> items(n);
+    for (int i = 0; i < n; i++)
     {
-        cout<<array[i]<<" ";
+        cin >> items[i];
     }
+    int start = 0;
+    int end = n - 1;
+    mergeSort(items, start, end);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << items[i] << " ";
+    }
+
     return 0;
 }
